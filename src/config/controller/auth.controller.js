ebe6d921f7912dcaad0139aa97b0cd11bfb98272
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-24 00:35:41 +0530

    feat(auth): implement user registration controller with validation and existence check
    feat(semver): add new range handling functions and utilities
    
    chore(deps): update package-lock.json with new dependencies including bcryptjs, cookie-parser, and jsonwebtoken
    
    fix(package): add dev script for nodemon in package.json

--- src/config/controller/auth.controller.js
+++ src/config/controller/auth.controller.js
@@ -6,7 +6,28 @@ const usreModel = require("../models/user.model")
  *  @access Public
  */
 
-async function registerUserController(req,res){}
+async function registerUserController(req,res){
+    const {username,email,password} = req.body
+    if(!username || !email || !password){
+        return res.status(400).json({
+            message: "Please provide username,email and password"
+        })
+    }
+
+    const isUserAlreadyExist = await usreModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(isUserAlreadyExist){
+
+        /* is user already exist with the same username or email address then return error response */
+        return res.status(400).json({
+            message: "Account already exist with this username or email address"
+        })
+    }
+}  
 
 module.exports = {
     registerUserController
