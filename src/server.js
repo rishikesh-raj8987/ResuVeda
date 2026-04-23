762a9cec070d9fdd80a644dcdfa0200967b334af
Rishikesh Raj <rishikeshthakur8987@gmail.com>
2026-04-24 00:01:01 +0530

    fix: correct database import path in server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const app = require("./src/app")
-const connectToDB =  require("./src/config/database")
+const connectToDB =  require("./src/config/database.js")
 
 app.listen(3000,()=>{
     console.log("Server  is stil is running on port 3000")
